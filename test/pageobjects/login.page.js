bef1d5b78a9794416a61597631116bac5a55e78e
faisaladdien <faisaladdien28@gmail.com>
2024-02-26 15:42:22 +0700

    E2E testing for login page

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -19,17 +19,43 @@ class LoginPage extends Page {
     get btnLogin () {
         return $('#login-button');
     }
+    //*[@id="login_button_container"]/div/form/div[3]/h3
+    //*[@id="login_button_container"]/div/form/div[3]/h3/text()
 
+    get errorLockedUser() { return $('//h3[text()="Epic sadface: Sorry, this user has been locked out."]')}
+    get errorNotRegisterdUser() { return $('//h3[text()="Epic sadface: Username and password do not match any user in this service"]')}
+    get errorBlankUsername() { return $('//h3[text()="Epic sadface: Username is required"]')}
+    get errorBlankPassword() { return $('//h3[text()="Epic sadface: Password is required"]')}
     /**
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
      */
     async login (username, password) {
+        await this.fieldUsername.waitForDisplayed({ timeout : 3000})
         await this.fieldUsername.setValue(username);
         await this.fieldPassword.setValue(password);
         await this.btnLogin.click();
     }
 
+    async validateLockedUser(){
+        await this.errorLockedUser.waitForDisplayed({ timeout : 3000})
+        await expect(this.errorLockedUser).toBeDisplayed()
+    }
+
+    async validateNotRegisteredUser(){
+        await this.errorNotRegisterdUser.waitForDisplayed({ timeout : 3000})
+        await expect(this.errorNotRegisterdUser).toBeDisplayed()
+    }
+
+    async validateBlankUsername(){
+        await this.errorBlankUsername.waitForDisplayed({ timeout : 3000})
+        await expect(this.errorBlankUsername).toBeDisplayed()
+    }
+
+    async validateBlankPassword(){
+        await this.errorBlankPassword.waitForDisplayed({ timeout : 3000})
+        await expect(this.errorBlankPassword).toBeDisplayed()
+    }
     /**
      * overwrite specific options to adapt it to page object
      */
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -10,5 +10,52 @@ describe('Login Sauce Demo', () => {
         await LoginPage.login(process.env.USERNAME_STANDARD_USER, process.env.PASSWORD_SAUCEDEMO)
         await HomePage.validateHomePage()
     })
+    it('should login with problem_user credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login(process.env.USERNAME_PROBLEM_USER, process.env.PASSWORD_SAUCEDEMO)
+        await HomePage.validateHomePage()
+    })
+    it('should login with performance_glitch_user credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login(process.env.USERNAME_PERFORMANCE_GLITCH_USER, process.env.PASSWORD_SAUCEDEMO)
+        await HomePage.validateHomePage()
+    })
+    it('should login with error_user credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login(process.env.USERNAME_ERROR_USER, process.env.PASSWORD_SAUCEDEMO)
+        await HomePage.validateHomePage()
+    })
+    it('should login with visual_user credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login(process.env.USERNAME_VISUAL_USER, process.env.PASSWORD_SAUCEDEMO)
+        await HomePage.validateHomePage()
+    })
+    //negative case with locked_out_user
+    it('should get error massage with locked_out_user credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login(process.env.USERNAME_LOCKED_OUT_USER, process.env.PASSWORD_SAUCEDEMO)
+        await LoginPage.validateLockedUser()
+    })
+    //negative case with not registered user
+    it('should get error massage with locked_out_user credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login(process.env.USERNAME_ANOTHER_USER, process.env.PASSWORD_SAUCEDEMO)
+        await LoginPage.validateNotRegisteredUser()
+    })
+    //negative case with blank field
+    it('should get error massage with blank username', async () => {
+        await LoginPage.open()
+        await LoginPage.login('', process.env.PASSWORD_SAUCEDEMO)
+        await LoginPage.validateBlankUsername()
+    })
+    it('should get error massage with blank password', async () => {
+        await LoginPage.open()
+        await LoginPage.login(process.env.USERNAME_STANDARD_USER,'')
+        await LoginPage.validateBlankPassword()
+    })
 })
 
